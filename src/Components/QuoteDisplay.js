b78bb09caf5957923f5942874f5ad6eb5a487d51
Rory Flint <roryflintphoto@gmail.com>
2020-04-03 10:56:55 +0100

    Add tests and dyanmically resize on content length

--- src/Components/QuoteDisplay.js
+++ src/Components/QuoteDisplay.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 function QuoteDisplay({ quote, author }) {
+  const resizeStyle = quote.length > 180 ? '2.9rem' : '3.5rem';
+  console.log(quote.length);
   return (
     <div className="quote-display">
-      <h1 id="text">{quote}</h1>
+      <h1 style={{fontSize: resizeStyle}} id="text">{quote}</h1>
       <h2 id="author">{author}</h2>
     </div>
   );
 }
+
 
 export default QuoteDisplay;
