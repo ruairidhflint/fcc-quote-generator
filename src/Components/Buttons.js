28e75858614e0a3561519fe6befcac8f97b19e4b
Rory Flint <roryflintphoto@gmail.com>
2020-04-03 10:20:57 +0100

    Complete button styles

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function Buttons() {
     return(
         <div class="buttons">
-            <a href='https://twitter.com/intent/tweet' id="tweet-quote"><i className="fas fa-twitter"></i></a>
+            <a href='https://twitter.com/intent/tweet' id="tweet-quote"><FontAwesomeIcon icon={faTwitter}/></a>
             <button id="new-quote">New Quote</button>
+            
         </div>
     )
 }
