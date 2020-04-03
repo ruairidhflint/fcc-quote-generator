93e9cc6d3b4661b314c276c9eceb982804b97873
Rory Flint <roryflintphoto@gmail.com>
2020-04-03 09:32:45 +0100

    Add MVP ID's

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Buttons() {
     return(
         <div class="buttons">
-            <button>Tweet</button>
-            <button>New Quote</button>
+            <a href='https://twitter.com/intent/tweet' id="tweet-quote">Tweet</a>
+            <button id="new-quote">New Quote</button>
         </div>
     )
 }
