176c4c4a69eb9f3c733fc2db297d144a65201531
Rory Flint <roryflintphoto@gmail.com>
2020-04-03 09:23:26 +0100

    Basic styling of quote box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.scss';
 
+// Component Imports
+import QuoteContainer from './Components/QuoteContainer';
+
 function App() {
   return (
     <div className="App">
+      <QuoteContainer />
     </div>
   );
 }
---
+++ src/Components/QuoteContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function QuoteContainer() {
+    return(
+        <div id='quote-box'>
+
+        </div>
+    )
+}
+
+export default QuoteContainer;
