d17704303c03ff2e2df928edb2c9b5569386ed83
Rory Flint <roryflintphoto@gmail.com>
2020-04-03 09:47:05 +0100

    Add FCC test script

--- src/Components/QuoteContainer.js
+++ src/Components/QuoteContainer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import QuoteDisplay from './QuoteDisplay'
+import Buttons from './Buttons'
+
 const dummyQuote = {
   quote: 'Life isn’t about getting and having, it’s about giving and being.',
   author: 'Kevin Kruse',
@@ -8,7 +11,8 @@ const dummyQuote = {
 function QuoteContainer() {
   return (
   <div id="quote-box">
-      
+      <QuoteDisplay quote={dummyQuote.quote} author={dummyQuote.author} />
+      <Buttons />
   </div>
   );
 
