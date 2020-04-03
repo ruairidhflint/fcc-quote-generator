f7fd678c28da258366d51ad02a86176f29085463
Rory Flint <roryflintphoto@gmail.com>
2020-04-03 10:33:44 +0100

    Tidy up

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function Buttons() {
-    return(
-        <div className="buttons">
-            <a href='https://twitter.com/intent/tweet' id="tweet-quote"><FontAwesomeIcon icon={faTwitter}/></a>
-            <button id="new-quote">New Quote</button>
-            
-        </div>
-    )
+  return (
+    <div className="buttons">
+      <a href="https://twitter.com/intent/tweet" id="tweet-quote">
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
+      <button id="new-quote">New Quote</button>
+    </div>
+  );
 }
 
 export default Buttons;
--- src/Components/QuoteContainer.js
+++ src/Components/QuoteContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import QuoteDisplay from './QuoteDisplay'
-import Buttons from './Buttons'
+import QuoteDisplay from './QuoteDisplay';
+import Buttons from './Buttons';
 
 const dummyQuote = {
   quote: 'Life isn’t about getting and having, it’s about giving and being.',
@@ -10,12 +10,11 @@ const dummyQuote = {
 
 function QuoteContainer() {
   return (
-  <div id="quote-box">
+    <div id="quote-box">
       <QuoteDisplay quote={dummyQuote.quote} author={dummyQuote.author} />
       <Buttons />
-  </div>
+    </div>
   );
-
 }
 
 export default QuoteContainer;
